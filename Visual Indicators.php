bf1a48a51b2100307db475705c9eacf19570fa31
Apocalypsecoder0 <Stephend8846@gmail.com>
2025-01-11 11:54:03 -0500

    Create Visual Indicators.php

---
+++ Visual Indicators.php
@@ -0,0 +1,5 @@
+<!-- Example of using icons for resource levels -->
+<div class="resource">
+    <img src="icons/energy.png" alt="Energy Icon">
+    <span><?php echo $resources['energy']; ?></span>
+</div>
