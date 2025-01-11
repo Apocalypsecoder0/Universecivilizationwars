6673ddb10b1b4d9c8a279fff159943b39d43a395
Apocalypsecoder0 <Stephend8846@gmail.com>
2025-01-11 11:33:10 -0500

    Create database.sql

---
+++ database.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE my_game;
+
+USE my_game;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
